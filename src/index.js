78bac583229d2682d5fc28c36634258ad1dfe973
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-05-21 11:52:05 -0400

    Document the TwoDimArray wrapper class

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 const funcs = require("./funcs");
 const is2DArray = require("is-2d-array");
 
+/**
+ * A class wrapper around the individual functions included in this library.
+ * Methods that return a 2D array can be chained.
+ */
 class TwoDimArray {
 	constructor(data) {
 		this.data = data;
 	}
 
+	/**
+	 * Builds a 2D array given dimensions and a value.
+	 * @param {number} rows The amount of rows (vertical, Y) to have.
+	 * @param {number} columns The amount of columns (horizontal, X) to have.
+	 * @param {*} defaultValue The value of each cell.
+	 * @returns {Array[]) The built 2D array.
+	 */
 	static build(rows, columns, defaultValue) {
 		return new Array(rows).fill().map(() => {
 			return new Array(columns).fill().map(() => {
