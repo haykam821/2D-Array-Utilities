8f61d9eb7caf713231dfd3556626e5427d22b69b
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-05-22 10:53:25 -0400

    Fix ESLint errors in test.js

--- test.js
+++ test.js
@@ -3,12 +3,11 @@
 const chai = require("chai");
 const assert = chai.assert;
 
-const TwoDimArray = require(".");
 const funcs = require("./src/funcs");
 
 const testTwoDimArray = [
-	[  1,  2,  3,  4,  5 ],
-	[  6,  7,  8,  9, 10 ],
+	[ 1, 2, 3, 4, 5 ],
+	[ 6, 7, 8, 9, 10 ],
 	[ 11, 12, 13, 14, 15 ],
 	[ 16, 17, 18, 19, 20 ],
 	[ 21, 22, 23, 24, 25 ],
@@ -16,17 +15,22 @@ const testTwoDimArray = [
 
 const funcTests = {
 	get: {
-		params: [3, 2],
 		assertToEqual: 14,
+		params: [3, 2],
 	},
 };
 
+/**
+ * Tests a given 2D array function.
+ * @param {string} name The name of the function.
+ * @param {function} func The function.
+ */
 function testFunction([ name, func ]) {
 	if (!funcTests[name]) return;
 
 	it(name, function() {
 		const test = funcTests[name];
-		assert.equal(funcs[name](...test.params), test.assertToEqual);
+		assert.equal(func(testTwoDimArray, ...test.params), test.assertToEqual);
 	});
 }
 
