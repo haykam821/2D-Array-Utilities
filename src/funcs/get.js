1897f5ef49de51568a14c8d2bea8ec537f15d087
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-05-21 22:02:14 -0400

    Document get and set functions

--- src/funcs/get.js
+++ src/funcs/get.js
@@ -1,3 +1,11 @@
-module.exports = (tda, column, row) => {
+/**
+ * Gets a cell in a 2D array by its coordinates.
+ * @param {(TwoDimArray|Array[])} tda The 2D array to get a cell's value from.
+ * @param {number} column The column (X) of the cell to get the value of.
+ * @param {number} row The row (Y) of the cell to get the value of.
+ * @returns {*} The value of the cell.
+ */
+function get(tda, column, row) {
 	return tda[row][column];
-};
+}
+module.exports = get;
--- src/funcs/set.js
+++ src/funcs/set.js
@@ -1,4 +1,13 @@
-module.exports = (tda, column, row, value) => {
+/**
+ * Sets the value of a cell in a 2D array.
+ * @param {(TwoDimArray|Array[])} tda The 2D array to set a cell in.
+ * @param {number} column The column (X) of the cell to set the value of.
+ * @param {number} row The row (Y) of the cell to set the value of.
+ * @param {*} value The new value of the cell.
+ * @returns {(TwoDimArray|Array[])} The updated 2D array.
+ */
+function set(tda, column, row, value) {
 	tda[row][column] = value;
 	return tda;
-};
+}
+module.exports = set;
